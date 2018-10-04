da65ad35ea7eafa174df783d946d7b612148369f
Taetse Durand <taetse.durand@gmail.com>
2018-10-04 15:58:20 +0200

    Table lookup

--- src/Main.java
+++ src/Main.java
@@ -11,15 +11,14 @@ public class Main {
 //        String fileName = strings[0];
         String fileName = "D:\\Documents\\COS 330\\assignments\\COS330Assignment5\\src\\CeasarText.txt";
 
-        char[] alphabet = ("0123456789" + "abcdefghijklmnopqrstuvwxyz" + "abcdefghijklmnopqrstuvwxyz".toUpperCase()).toCharArray();
-        short[] alphabetIndex = new short[128];
-        for (short a = 0; a < alphabetIndex.length; a++) {
-            alphabetIndex[a] = -1;
-            for (short b = 0; b < alphabet.length; b++) {
-                if (alphabet[b] == a)
-                    alphabetIndex[a] = b;
-            }
-        }
+        final char[] alphabet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+                'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+        final short[] alphabetIndex = new short[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+                -1, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, -1, -1, -1, -1, -1, -1, -1, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46,
+                47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, 10, 11, 12, 13, 14, 15, 16,
+                17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, -1, -1, -1, -1, -1 };
 
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
@@ -59,12 +58,12 @@ public class Main {
     }
 
     private static HashSet<String> getDictionary(String path) {
-        HashSet<String> dictionary = new HashSet<>();
+        HashSet<String> dictionary = new HashSet<>(70000);
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
             String line;
             while((line = bufferedReader.readLine())!= null)
-                dictionary.add(line.toLowerCase());
+                dictionary.add(line);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
